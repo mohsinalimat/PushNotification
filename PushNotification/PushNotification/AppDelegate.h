b9b3f9a5361e8bf86e47c5060f8e5b1996970d58
Ravi Dhorajiya <dhorajiya104@gmail.com>
2017-07-13 11:56:53 +0530

    PushNotification

---
+++ PushNotification/PushNotification/AppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.h
+//  PushNotification
+//
+//  Created by wos on 13/07/17.
+//  Copyright © 2017 Ravi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <UserNotifications/UserNotifications.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate,UNUserNotificationCenterDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PushNotification/PushNotification/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  PushNotification
+//
+//  Created by wos on 13/07/17.
+//  Copyright © 2017 Ravi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
